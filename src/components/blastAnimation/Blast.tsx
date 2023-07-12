90477eaaa491900351938a164437a15e08e31b5a
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-07-12 20:17:02 +0900

    feat: add Blast tsx components

---
+++ src/components/blastAnimation/Blast.tsx
@@ -0,0 +1,17 @@
+import './blast.scss';
+
+const Blast = ({ letterClass, arrayStr, indexLetter }) => {
+  return (
+    <span>
+      {arrayStr.map((char, index) => {
+        return (
+          <span key={char + index} className={`${letterClass} _${index + indexLetter}`}>
+            {char}
+          </span>
+        );
+      })}
+    </span>
+  );
+};
+
+export default Blast;
