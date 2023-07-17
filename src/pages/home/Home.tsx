6acade741eaeaf3e1833a0bd5256f30022115d90
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-07-17 21:48:45 +0900

    feat: add Home tsx page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import About from '../about/About';
+
+const Home = () => {
+  return (
+    <>
+      <section className='section-about'>
+        <About />
+      </section>
+    </>
+  );
+};
+
+export default Home;
