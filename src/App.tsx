e4415380444a3beda7728547f12ae726bc1f3afd
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-07-19 14:57:50 +0900

    feat: add routes inside App tsx page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { About } from './pages';
+
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const location = useLocation();
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </Router>
+    <Routes location={location} key={location.pathname}>
+      <Route path='/about' element={<About />} />
+    </Routes>
   );
 }
 
