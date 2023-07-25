49c86202c8d19b240cc3053580c7f037afbf71a3
Changsu PARK <61610843+changsu1993@users.noreply.github.com>
2023-07-25 16:18:27 +0900

    feat: add @emailjs/browser library

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Cards } from './skillsCards/Cards';
 export { default as Blast } from './blastAnimation/Blast';
 export { default as Header } from './header/Header';
 export { default as Projects } from './projects/Projects';
+export { default as Map } from './map/Map';
